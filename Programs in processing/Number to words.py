b5187c7be7895c4acc11bf7851c6f68a81bbc0d1
razaali11110 <60029512+razaali11110@users.noreply.github.com>
2020-01-25 00:54:59 +0500

    Add files via upload

---
+++ Programs in processing/Number to words.py
@@ -0,0 +1,5 @@
+dic1 = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
+dic2 = {11: "Eleven", 12: "Twelve", 13: "thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen",
+        17: "Seventeen", 18: "Eighteen", 19: "Nineteen"}
+dic3 = {10: "Ten", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy", 80: "Eighty",
+        90: "Ninety", 100: "Hundred"}
